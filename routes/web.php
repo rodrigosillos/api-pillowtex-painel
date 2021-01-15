910b735f49ee11051502388b588329a236eedf4e
Rodrigo Sillos <rodrigosillos@gmail.com>
2021-01-15 14:10:20 -0300

    routes folder

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+
+// Route::get('{any}', 'HomeController@index');
+
+Route::get('/comissoes', 'CommissionsController@index');
+
+//Language Translation
+Route::get('index/{locale}', 'HomeController@lang');
+
+//Socialite 
+// Route::get('auth/{provider}', 'Auth\RegisterController@redirectToProvider');
+// Route::get('auth/{provider}/callback', 'Auth\RegisterController@handleProviderCallback');
+
+Route::get('/', 'HomeController@root');

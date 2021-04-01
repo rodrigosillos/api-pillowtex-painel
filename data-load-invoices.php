51c27ddc2e5107e3513a0471bc564bd11b7468f3
Rodrigo Sillos <rodrigosillos@gmail.com>
2021-04-01 14:32:06 -0300

    data load

--- data-load-invoices.php
+++ data-load-invoices.php
@@ -6,8 +6,8 @@ include('connection-db.php');
 $operationType = 'S'; // Entrada (Dedução) / Saida (Faturamento 50% / Substituição / Liquidição)
 
 $dataListaMovimentacao = [
-    'datai' => '2021-01-01',
-    'dataf' => '2021-01-10',
+    'datai' => '2020-10-01',
+    'dataf' => '2021-04-01',
     '$format' => 'json',
     'tipo_operacao' => $operationType,
 ];

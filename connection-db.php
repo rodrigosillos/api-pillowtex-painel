43eda598c31f389a664fd117d1200a36e134b3e8
Rodrigo Sillos <rodrigosillos@gmail.com>
2021-04-28 12:03:49 -0300

    fix conn db

--- connection-db.php
+++ connection-db.php
@@ -1,4 +1,4 @@
 <?php
 
-$pdo = new PDO('mysql:host=db;dbname=pillowtex', 'root', 'qcLkozSAB3L4rp2TTUN7rJVlJa9C1CTb9hcdSLhcuiA=');
+$pdo = new PDO('mysql:host=db;dbname=pillowtex', 'root', 'a9Xn/Gb7vkxdLaM38z7eVjgtPcuP+SKToyLdro5Yh4E==');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);

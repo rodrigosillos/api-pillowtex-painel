cfdd8ce1f6083a82dd860feca08fb8ec05209f2e
Rodrigo Sillos <rodrigosillos@gmail.com>
2021-07-29 08:03:59 -0300

    commission debtors

--- cron-recalculate-commission-debtors.php
+++ cron-recalculate-commission-debtors.php
@@ -3,8 +3,8 @@
 include('call-api.php');
 include('connection-db.php');
 
-//$sql = "select operation_code, operation_type, commission_amount from invoices where issue_date between '2021-07-09' and '2021-07-23'";
-$sql = "select operation_code, operation_type, commission_amount from invoices where agent_id = 263";
+$sql = "select operation_code, operation_type, commission_amount from invoices where issue_date between '2021-02-01' and '2021-07-31'";
+//$sql = "select operation_code, operation_type, commission_amount from invoices where agent_id = 263";
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
 $invoices = $stmt->fetchAll();
@@ -14,9 +14,9 @@ foreach ($invoices as $invoice__) {
     $operationCode = $invoice__["operation_code"];
     $commissionAmount = $invoice__["commission_amount"];
 
-    $qtyDebtors = $pdo->query('select count(*) from debtors where substituted = 0 and operation_code = '.$operationCode)->fetchColumn(); 
+    $qtyDebtors = $pdo->query('select count(*) from debtors where substituted = 0 and operation_code = '.$operationCode)->fetchColumn();
 
-    $sql = "select id from debtors where substituted = 0 and operation_code = :operation_code";
+    $sql = "select id from debtors where substituted = 0 and commission = 0 and operation_code = :operation_code";
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':operation_code', $operationCode, PDO::PARAM_STR);
     $stmt->execute();
@@ -26,7 +26,7 @@ foreach ($invoices as $invoice__) {
 
         $debtorId = $debtor__["id"];
         $debtorCommission = (($commissionAmount / 2) / $qtyDebtors);
-        //print($bookEntryCommission . "\xA");
+        print($debtorCommission . "\xA");
 
         $data = [
             'commission' => $debtorCommission,

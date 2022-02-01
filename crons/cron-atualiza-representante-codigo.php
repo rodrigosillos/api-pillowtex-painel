d60db3ad8ba2eb92295c4d0d0e8a6fde086bc0e8
Rodrigo Sillos <rodrigosillos@gmail.com>
2022-02-01 19:42:43 -0300

    fix liquidacao

--- crons/cron-atualiza-representante-codigo.php
+++ crons/cron-atualiza-representante-codigo.php
@@ -1,7 +1,7 @@
 <?php
 
-include('call-api.php');
-include('connection-db.php');
+// include('call-api.php');
+include('../connection-db.php');
 
 $sql = "select u.agent_id2, u.agent_code, l.numero_lancamento from lancamentos l left join users u on l.representante = u.agent_id2 where l.representante_codigo is null";
 $stmt = $pdo->prepare($sql);

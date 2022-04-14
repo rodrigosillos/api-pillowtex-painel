8f7d8598ec330ce00251d750680a5e472969c184
Rodrigo Sillos <rodrigosillos@gmail.com>
2022-04-14 18:07:11 -0300

    nova tabela produtos

--- crons/cron-comissao-dividida-liquidacao.php
+++ crons/cron-comissao-dividida-liquidacao.php
@@ -1,6 +1,6 @@
 <?php
 
-include('call-api.php');
+include('call-api-novo.php');
 include('connection-db.php');
 
 $sql = "select n_documento, origem from titulos_receber where data_pagamento between '2022-03-01' and '2022-03-31'";
@@ -13,8 +13,22 @@ foreach ($titulosReceber as $tituloReceber) {
     $tituloNumero = explode('/', $tituloReceber["n_documento"]);
     $origem = $tituloReceber["origem"];
 
-    $numeroParcelas = $pdo->query("select count(*) from titulos_receber where n_documento like '%".$tituloNumero[0]."%'")->fetchColumn();
-    print('n_documento: ' . $tituloNumero[0] . ' - origem: ' . $origem . ' - n parcelas: ' . $numeroParcelas . "\xA");
+    if($origem) {
+
+        $sql = "select valor_comissao from movimentacao where cod_operacao = :cod_operacao";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':cod_operacao', $origem, PDO::PARAM_STR);
+        $stmt->execute();
+        $movimentacao = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+        $valorComissao = 0;
+
+        if ($stmt->rowCount() > 0)
+            $valorComissao = $movimentacao['valor_comissao'];
+
+        $numeroParcelas = $pdo->query("select count(*) from titulos_receber where n_documento like '%".$tituloNumero[0]."%'")->fetchColumn();
+        print('n_documento: ' . $tituloNumero[0] . ' - origem: ' . $origem . ' - n parcelas: ' . $numeroParcelas . ' - valor comissão: ' . $valorComissao . "\xA");
+    } 
 
 }
 
---
+++ database/migrations/2022_04_14_182724_create_produtos_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('cod_operacao');
+            $table->integer('produto');
+            $table->string('cod_produto', 60);
+            $table->string('descricao1', 100);
+            $table->integer('divisao');
+            $table->integer('cod_divisao');
+            $table->integer('descricao_divisao');
+            $table->integer('cor');
+            $table->integer('estampa');
+            $table->string('tamanho', 5);
+            $table->integer('quantidade');
+            $table->float('preco', 8, 2)->default(0.00);
+            $table->float('ipi', 8, 2)->default(0.00);
+            $table->integer('pedido');
+            $table->string('unidade', 5);
+            $table->integer('nota');
+            $table->float('preco_aplicado', 8, 2)->default(0.00);
+            $table->float('desconto', 8, 2)->default(0.00);
+            $table->float('preco_bruto', 8, 2)->default(0.00);
+            $table->float('valor_comissao', 8, 2)->default(0.00);
+            $table->float('porcentagem_comissao', 8, 2)->default(0.00);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}

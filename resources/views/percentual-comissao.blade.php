26ae077f24533b27b1b331e550d9d90fac59b774
Rodrigo Sillos <rodrigosillos@gmail.com>
2022-04-28 11:48:57 -0300

    fix telas percentual

--- resources/views/percentual-comissao.blade.php
+++ resources/views/percentual-comissao.blade.php
@@ -27,17 +27,17 @@
                                 
                                 <div class="form-group col-lg-2">
                                     <label for="tabela_{{ $key }}">Tabela de Preço</label>
-                                    <input type="text" id="tabela_{{ $key }}" name="tabela" class="form-control" value="{{ $value['tabela'] }}"/>
+                                    <input type="text" id="tabela_{{ $key }}" name="tabela" readonly class="form-control" value="{{ $value['tabela'] }}"/>
                                 </div>
 
                                 <div class="form-group col-lg-2">
                                     <label for="cod_divisao_{{ $key }}">Cod Divisão</label>
-                                    <input type="text" id="cod_divisao_{{ $key }}" name="cod_divisao" class="form-control" value="{{ $value['cod_divisao'] }}"/>
+                                    <input type="text" id="cod_divisao_{{ $key }}" name="cod_divisao" readonly class="form-control" value="{{ $value['cod_divisao'] }}"/>
                                 </div>
 
                                 <div class="form-group col-lg-2">
                                     <label for="descricao_divisao_{{ $key }}">Descrição Divisão</label>
-                                    <input type="text" id="descricao_divisao_{{ $key }}" name="descricao_divisao" class="form-control" value="{{ $value['descricao_divisao'] }}"/>
+                                    <input type="text" id="descricao_divisao_{{ $key }}" name="descricao_divisao" readonly class="form-control" value="{{ $value['descricao_divisao'] }}"/>
                                 </div>
 
                                 <div class="form-group col-lg-2">
@@ -45,9 +45,9 @@
                                     <input type="text" id="percentual_comissao_{{ $key }}" name="percentual_comissao" class="form-control" value="{{ $value['percentual_comissao'] }}"/>
                                 </div>
                                 
-                                <div class="col-lg-2 align-self-center">
+                                <!-- <div class="col-lg-2 align-self-center">
                                     <input data-repeater-delete type="button" class="btn btn-primary btn-block" value="Apagar"/>
-                                </div>
+                                </div> -->
                                 
                             </div>
                             @endforeach

ee4c9757f45c1d4a5b316384b4e11f7a854cf9c3
Rodrigo Sillos <rodrigosillos@gmail.com>
2022-06-02 14:06:14 -0300

    recarga maio #2

--- crons/cron-ajusta-movimento-especial.php
+++ crons/cron-ajusta-movimento-especial.php
@@ -8,8 +8,8 @@ include('connection-db.php');
 $sql = "
 select m.romaneio, m.cod_operacao, m.tipo_pedido, m.notas
 from movimentacao m 
-where m.notas = '' and m.data_emissao between '2022-05-27' and '2022-05-31'
-or m.notas is null and m.data_emissao between '2022-05-27' and '2022-05-31'
+where m.notas = '' and m.data_emissao between '2022-05-31' and '2022-05-31'
+or m.notas is null and m.data_emissao between '2022-05-31' and '2022-05-31'
 ";
 
 // $sql = "select m.cod_operacao from movimentacao m where m.notas is null and m.tipo_pedido is null and m.representante_cod = '0054' and m.data_emissao between '2022-05-01' and '2022-05-20'";

eb3b2397ec85d479a635bac258ab6476fe4e658d
Rodrigo Sillos <rodrigosillos@gmail.com>
2022-06-09 18:53:19 -0300

    liqui fama

--- crons/modulo-liquidacao.php
+++ crons/modulo-liquidacao.php
@@ -6,7 +6,7 @@ include('connection-db.php');
 $parametros = [
     'efetuado' => 'true',
     'substituido' => 'false',
-    'representante' => '6',
+    'representante' => '229',
     '$format' => 'json',
     '$dateformat' => 'iso',
     'tipo' => 'R',

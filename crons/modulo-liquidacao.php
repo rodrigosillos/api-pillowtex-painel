c464699dcf30bdff11de02c0b20166fcee24359d
Rodrigo Sillos <rodrigosillos@gmail.com>
2022-06-09 18:09:39 -0300

    recarrega liquadacao camena

--- crons/modulo-liquidacao.php
+++ crons/modulo-liquidacao.php
@@ -6,7 +6,7 @@ include('connection-db.php');
 $parametros = [
     'efetuado' => 'true',
     'substituido' => 'false',
-    'representante' => '5',
+    'representante' => '6',
     '$format' => 'json',
     '$dateformat' => 'iso',
     'tipo' => 'R',

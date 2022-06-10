2511d359523840b3f36f4ec02ddc801bed19aced
Rodrigo Sillos <rodrigosillos@gmail.com>
2022-06-10 13:48:32 -0300

    calculo comissao liqui #2

--- crons/cron-atualiza-titulo-sem-cliente.php
+++ crons/cron-atualiza-titulo-sem-cliente.php
@@ -2,7 +2,7 @@
 
 include('connection-db.php');
 
-$sql = "select id, n_documento, origem from titulos_receber where cliente_nome = ''";
+$sql = "select id, n_documento, origem from titulos_receber where cliente_nome = '' and data_pagamento between '2022-05-01' and '2022-05-31'";
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
 $titulosReceber = $stmt->fetchAll();

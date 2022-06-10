4ee129cf0d25ce0ad3cd3762917f9ab2e5a278a1
Rodrigo Sillos <rodrigosillos@gmail.com>
2022-06-10 14:44:34 -0300

    liqui 0041 base antiga

---
+++ crons/cron-comissao-dividida-liquidacao-base-antiga.php
@@ -0,0 +1,53 @@
+<?php
+
+include('call-api-novo.php');
+include('connection-db.php');
+
+$sql = "select id, n_documento, origem from titulos_receber where data_pagamento between '2022-05-01' and '2022-05-31' and valor_comissao = 0";
+// $sql = "select id, n_documento, origem from titulos_receber where origem = 8803";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$titulosReceber = $stmt->fetchAll();
+
+foreach ($titulosReceber as $tituloReceber) {
+
+    $tituloID = $tituloReceber["id"];
+    $tituloNumero = explode('/', $tituloReceber["n_documento"]);
+    $origem = $tituloReceber["origem"];
+    
+    $valorComissao = 0;
+
+    if($origem) {
+
+        $sql = "select issue_date, commission_amount from invoices where operation_code = :cod_operacao";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':cod_operacao', $origem, PDO::PARAM_STR);
+        $stmt->execute();
+        $movimentacao = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+        if($stmt->rowCount() > 0) {
+
+            $valorComissao = $movimentacao['commission_amount'];
+            $numeroParcelas = $pdo->query("select count(*) from titulos_receber where n_documento like '".$tituloNumero[0]."%'")->fetchColumn();
+            
+            if($valorComissao > 0) {
+                
+                print('n parcelas: ' . $numeroParcelas . ' - valor comissão: ' . $valorComissao . ' - comissão título: ' . ( ( $valorComissao / 2 ) / $numeroParcelas ) .  "\xA");
+
+                $data = [
+                    'valor_comissao' => ( ( $valorComissao / 2 ) / $numeroParcelas ),
+                    'id' => $tituloID,
+                ];
+            
+                $sql = "update titulos_receber set valor_comissao = :valor_comissao where id = :id";
+                $stmt = $pdo->prepare($sql);
+                $stmt->execute($data);
+            }
+                
+        }
+
+    } 
+
+}
+
+$pdo = null;

b5e697e97cda5d3b39aaa0fa55cdffa26bb08605
Rodrigo Sillos <rodrigosillos@gmail.com>
2022-06-16 16:11:03 -0300

    evento 213

--- crons/cron-comissao-dividida-faturamento.php
+++ crons/cron-comissao-dividida-faturamento.php
@@ -4,7 +4,7 @@ include('call-api.php');
 include('connection-db.php');
 
 // $sql = "select m.cod_operacao, m.tipo_operacao, m.cliente_estado, m.tabela, m.tipo_pedido, m.data_emissao, m.comissao_r, m.representante, m.representante_cliente from movimentacao m where m.notas is null and m.representante_cod = '0054' and m.data_emissao between '2022-05-01' and '2022-05-20";
-$sql = "select m.cod_operacao, m.tipo_operacao, m.cliente_estado, m.tabela, m.tipo_pedido, m.data_emissao, m.comissao_r, m.representante, m.representante_cliente from movimentacao m where m.data_emissao between '2022-05-01' and '2022-05-31'";
+$sql = "select m.cod_operacao, m.tipo_operacao, m.cliente_estado, m.tabela, m.tipo_pedido, m.data_emissao, m.comissao_r, m.representante, m.representante_cliente, m.evento from movimentacao m where m.data_emissao between '2022-05-01' and '2022-05-31'";
 
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
@@ -23,6 +23,7 @@ foreach ($invoices as $invoice) {
     $comissaoR = $invoice['comissao_r'];
     $representante = $invoice['representante'];
     $representanteCliente = $invoice['representante_cliente'];
+    $evento = $invoice['evento'];
 
     $issueDate = date_create($issueDate);
 
@@ -67,7 +68,7 @@ foreach ($invoices as $invoice) {
         if($tableCode == 187 && $clientAddress != 'SP' && $discount < 5)
             $commissionPercentage = 3;
 
-        if ($invoiceType == 'ZC PEDIDO ESPECIAL')
+        if ($invoiceType == 'ZC PEDIDO ESPECIAL' || $evento = 213)
             $commissionPercentage = $comissaoR;
         
         $priceProduct = $priceApplied == 0 ? $price : $priceApplied;

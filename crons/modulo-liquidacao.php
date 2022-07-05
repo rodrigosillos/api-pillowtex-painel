6d21771eb733ee0a7ebeeb360ae09860cdd057e3
Rodrigo Sillos <rodrigosillos@gmail.com>
2022-07-05 15:36:11 -0300

    titulos #2

--- crons/modulo-liquidacao.php
+++ crons/modulo-liquidacao.php
@@ -6,7 +6,7 @@ include('connection-db.php');
 $parametros = [
     // 'efetuado' => 'true',
     // 'substituido' => 'false',
-    'representante' => '7',
+    // 'representante' => '7',
     '$format' => 'json',
     '$dateformat' => 'iso',
     'tipo' => 'R',

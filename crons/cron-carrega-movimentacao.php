f151d1ce4bc6a6e9c12d8588d293c81c3a499b6a
Rodrigo Sillos <rodrigosillos@gmail.com>
2023-09-05 09:19:54 -0300

    final agosto

--- crons/cron-carrega-movimentacao.php
+++ crons/cron-carrega-movimentacao.php
@@ -10,8 +10,8 @@ $tiposOperacao = ['S', 'E']; // Entrada (Dedução) / Saida (Faturamento 50% / S
     $tipoOperacao = 'S';
     
     $paramsListaMovimentacao = [
-        'datai' => '2023-08-24',
-        'dataf' => '2023-08-29',
+        'datai' => '2023-08-30',
+        'dataf' => '2023-08-31',
         // 'representante' => '5',
         '$format' => 'json',
         'tipo_operacao' => $tipoOperacao,
